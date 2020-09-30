b98b96c646ccb43c909fb9c838a5435c47d27301
alaketych <alaketych@ukr.net>
2020-09-30 21:20:22 +0300

    react components

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classNames from 'classnames'
+
+function Button({ label, className }) {
+    return (
+        <button className={classNames('button', className)}>
+            { label }
+        </button>
+    )
+}
+
+export default Button
