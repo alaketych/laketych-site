8b1f7a3748c455678e7ae65e9be54c72e682cb38
alaketych <alaketych@ukr.net>
2020-10-18 22:30:30 +0300

    express server

--- server-rendering/server.js
+++ server-rendering/server.js
@@ -1,14 +1,38 @@
-const port = 8000
+// const port = 8000
+// const express = require('express')
+// const server = express()
+
+// const posts = require('./routes/posts')
+// const projects = require('./routes/projects')
+
+// server.use('/posts', posts)
+// server.use('/projects', projects)
+
+// server.listen(port, error => {
+//     if(error) throw error
+//     console.log(`> Ready on http://localhost:${port}`);
+// })
+
 const express = require('express')
-const server = express()
+const next = require('next')
+const routes = require('./routes')
 
-const posts = require('./routes/posts')
-const projects = require('./routes/projects')
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const handler = routes.getRequestHandler(app)
 
-server.use('/posts', posts)
-server.use('/projects', projects)
+app.prepare()
+.then(() => {
+  const server = express()
+  server.use(handler)
 
-server.listen(port, error => {
-    if(error) throw error
-    console.log(`> Ready on http://localhost:${port}`);
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
 })
