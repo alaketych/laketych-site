a3c317d9b8d0985dee3d06e0eb43eac546cbc380
alaketych <alaketych@ukr.net>
2020-10-18 22:35:01 +0300

    express routes

--- server-rendering/routes/index.js
+++ server-rendering/routes/index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const database = require('../pages/api/controllers/database')
+// const express = require('express')
+// const database = require('../pages/api/controllers/database')
 
-const router = express.Router()
+// const router = express.Router()
 
-module.exports = { router, database }
+// module.exports = { router, database }
+
+const nextRoutes = require('next-routes')
+const route = module.exports = nextRoutes()
+
+route.add('posts', '/posts')
+route.add('post', '/posts/:slug')
