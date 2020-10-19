8d2ed956302bf9280db9be9a6c2d8587b70deb4a
alaketych <alaketych@ukr.net>
2020-10-19 22:46:18 +0300

    change fetch data

--- server-rendering/pages/api/controllers/fetching.js
+++ server-rendering/pages/api/controllers/fetching.js
@@ -1,9 +1,9 @@
 import fetch from 'isomorphic-unfetch'
 
 export function getPosts() {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
+    return fetch('http://localhost:3000/api/posts')
 }
 
 export function getPost(slug) {
-    return fetch(`https://jsonplaceholder.typicode.com/posts?id=${slug}`)
+    return fetch(`http://localhost:3000/api/posts/${slug}`)
 }
