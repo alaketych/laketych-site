e2b8628001722c075aea7a72f094268b20e36b5f
alaketych <alaketych@ukr.net>
2020-11-05 11:34:17 +0200

    delete posts & projects

--- server-rendering/pages/api/posts/index.js
+++
@@ -1,8 +0,0 @@
-import db from '../controllers/database'
-
-export default async(request, response) => {
-    const sql = `select * from post`
-    const posts = await db.query(sql)
-
-    response.status(200).json(posts)
-}
--- server-rendering/pages/api/projects/index.js
+++
@@ -1,8 +0,0 @@
-import db from '../controllers/database'
-
-export default async(request, response) => {
-    const sql = `select * from project`
-    const projects = await db.query(sql)
-
-    response.status(200).json(projects)
-}
